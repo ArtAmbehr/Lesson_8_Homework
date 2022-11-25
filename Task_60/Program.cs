0765ab6d8b0c196ab7f135d82fe94627431a3826
Art <art-75@yandex.ru>
2022-11-25 23:02:10 +0300

    Task 60

---
+++ Task_60/Program.cs
@@ -0,0 +1,62 @@
+﻿// // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// // Массив размером 2 x 2 x 2
+// // 66(0,0,0) 25(0,1,0)
+// // 34(1,0,0) 41(1,1,0)
+// // 27(0,0,1) 90(0,1,1)
+// // 26(1,0,1) 55(1,1,1)
+
+Console.Write("Введите количество элементов длины массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество элементов ширины массива: ");
+int columns = int.Parse(Console.ReadLine());
+Console.Write("Введите количество элементов высоты массива: ");
+int plane = int.Parse(Console.ReadLine());
+
+int[,,] array = GetArray(rows, columns, plane, 10, 99);
+PrintArray(array);
+
+int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[x, y, z];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+            }
+        }
+    }
+    return result;
+}
+
+
+bool FindElement(int[,,] array, int el)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == el) return true;
+            }
+        }
+    }
+    return false;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();    
+        }
+    }
+}
